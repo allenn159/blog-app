0a43eb2b4533a6f6aa6411635f3e4b63f6824074
allenn159 <allenn159@gmail.com>
2021-08-27 23:34:07 -0700

    Added reducer to add blogs

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,20 +1,51 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { addNewBlog } from "../features/counterSlice";
 import AddIcon from "@material-ui/icons/Add";
+import { v4 as uuidv4 } from "uuid";
 
 const Frontpage = () => {
+  const dispatch = useDispatch();
   const blogs = useSelector((state) => state.blogs.blogCollection);
+  const [newTitle, setNewTitle] = useState("");
+  const [newBody, setNewBody] = useState("");
+
+  console.log(blogs);
+
+  const onSubmitNewBlog = (e) => {
+    e.preventDefault();
+
+    if (newTitle === "" && newBody === "") return;
+
+    const newBlog = {
+      id: uuidv4(),
+      title: newTitle,
+      body: newBody,
+      favorite: false,
+    };
+
+    dispatch(addNewBlog(newBlog));
+    setNewTitle("");
+    setNewBody("");
+  };
 
   return (
     <div>
       {blogs.map((b) => (
-        <div>
+        <div key={b.id}>
           <h1>{b.title}</h1>
           <p>{b.body}</p>
         </div>
       ))}
-      <input></input>
-      <input></input>
+      <input
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+      ></input>
+      <input
+        value={newBody}
+        onChange={(e) => setNewBody(e.target.value)}
+      ></input>
+      <AddIcon onClick={onSubmitNewBlog} />
     </div>
   );
 };
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   blogCollection: [
     {
+      id: 1,
       title: "Blog Title One",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       favorite: false,
     },
     {
+      id: 2,
       title: "Blog Title Two",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       favorite: false,
@@ -18,7 +20,17 @@ const initialState = {
 export const counterSlice = createSlice({
   name: "blogs",
   initialState,
-  reducers: {},
+  reducers: {
+    addNewBlog: (state, action) => {
+      // state.blogCollection.push(action.payload);
+      return {
+        ...state,
+        blogCollection: [...state.blogCollection, action.payload],
+      };
+    },
+  },
 });
+
+export const { addNewBlog } = counterSlice.actions;
 
 export default counterSlice.reducer;
